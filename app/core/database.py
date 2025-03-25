cb41f3e95ee1e25c467d60f04f8563599943b331
@aleksandr_turshiev <240720008@mail.ru>
2025-03-26 05:28:08 +0700

    Add db session maker + core init

---
+++ app/core/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from config import Config
+
+
+engine = create_async_engine(Config.DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
