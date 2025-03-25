c43c0f77cddd7a6d84cb79a2d8d6112a2f90b79b
@aleksandr_turshiev <240720008@mail.ru>
2025-03-26 06:03:28 +0700

    Fix account/payment mistake

--- app/routes/payment.py
+++ app/routes/payment.py
@@ -2,9 +2,9 @@ from sanic import Blueprint
 from sanic.response import json
 
 
-account_bp = Blueprint("account")
+payment_bp = Blueprint("payment")
 
 
-@account_bp.get("/accounts")
+@payment_bp.get("/accounts")
 async def get_accounts(request):
-    return json({"message": "User accounts list will be implemented here"})
+    return json({"message": "Payment logic will be implemented here"})
