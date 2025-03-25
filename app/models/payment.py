b0df4e9146214baafbbdc4c1753dabaea075ff6a
@aleksandr_turshiev <240720008@mail.ru>
2025-03-26 05:40:02 +0700

    Add payment model

---
+++ app/models/payment.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from base import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    transaction_id = Column(String, unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    account_id = Column(Integer, ForeignKey("accounts.id"), nullable=False)
+    amount = Column(Integer, nullable=False)
+
+    user = relationship("User")
+    account = relationship("Account")
