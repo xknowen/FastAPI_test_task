c33bf10a1093dc7517571581a942d63af7720392
@aleksandr_turshiev <240720008@mail.ru>
2025-03-26 06:04:52 +0700

    Add routes blueprints

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from sanic import Sanic
 from sanic.response import json
 from core.database import engine
 from models.base import Base
+from routes.auth import auth_bp
+from routes.user import user_bp
+from routes.admin import admin_bp
+from routes.account import account_bp
+from routes.payment import payment_bp
 
 
 app = Sanic("TestTaskSanic")
@@ -11,6 +16,13 @@ app = Sanic("TestTaskSanic")
 async def setup_db(app, loop):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+app.blueprint(auth_bp)
+app.blueprint(user_bp)
+app.blueprint(admin_bp)
+app.blueprint(account_bp)
+app.blueprint(payment_bp)
 
 
 @app.get("/")
